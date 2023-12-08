5069c0edac114e4b680eb27ce4b48b91a4edfdf9
Daniel Bauer <dan717@gmx.de>
2023-12-08 14:37:28 +0100

    Added functionality to write the ithub user to json file. But all requests are sent  twice

--- recipe-frontend/src/utils/api.js
+++ recipe-frontend/src/utils/api.js
@@ -1,1 +1,17 @@
-export const api = "http://localhost:9000";
+import axios from 'axios';
+
+// export const api = "http://localhost:9000";
+
+// Schreibe eine Funktion die User Daten aus einer Session empfängt und an einen externen Express Server schickt:
+export const sendUserData = async (session) => {
+	try {
+		const response = await axios.post(`http://localhost:5000/auth/signup`, {
+			name: session.user.name,
+			email: session.user.email,
+			loginMethod: 'github',
+		});
+		console.log('[sendUserData] response.data:', response.data);
+	} catch (error) {
+		console.error('[sendUserData] error:', error);
+	}
+};
