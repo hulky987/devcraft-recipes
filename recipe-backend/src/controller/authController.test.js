a814b78f0c837e8b0e3e48fb06aa45efbe0c01eb
hulky987 <yasin.guel@web.de>
2023-12-12 13:32:50 +0100

    fix config path

--- recipe-backend/src/controller/authController.test.js
+++ recipe-backend/src/controller/authController.test.js
@@ -1,7 +1,7 @@
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
-require('dotenv').config({ path: `.env.test` });
+require('dotenv').config({ path: `../../.env.${process.env.NODE_ENV}` });
 
 const {
 	beforeAll,
