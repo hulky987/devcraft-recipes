2fd6c636d38957d7400bbfc0505fb24b7529240e
Daniel Bauer <dan717@gmx.de>
2023-12-12 17:56:27 +0100

    Unit User Model tests with Prisma and test database completed

--- recipe-backend/src/models/userModel.js
+++ recipe-backend/src/models/userModel.js
@@ -204,14 +204,23 @@ signupUserModel = async (name, email, password, loginMethod) => {
 // };
 
 loginUserModel = async (email, password) => {
+	// console.log('[loginUserModel] function called');
 	const user = await prisma.UserLocal.findFirst({
 		where: { email: email },
 	});
+	// console.log(
+	// 	'[loginUserModel] user after prisma.UserLocal.findFirst: ',
+	// 	user
+	// );
 	if (!user) {
 		return null;
 	}
 
 	const isPasswordValid = bcrypt.compareSync(password, user.password);
+	// console.log(
+	// 	'[loginUserModel] isPasswordValid after bcrypt.compareSync: ',
+	// 	isPasswordValid
+	// );
 	if (!isPasswordValid) {
 		return null;
 	}
