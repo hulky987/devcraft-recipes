6204968bc1fb1a9805447379f07a402ba17320a3
Daniel Bauer <dan717@gmx.de>
2023-12-13 13:38:12 +0100

    Done adding functtionality to set user and cookie  in storages

--- recipe-backend/src/models/userModel.js
+++ recipe-backend/src/models/userModel.js
@@ -208,19 +208,19 @@ loginUserModel = async (email, password) => {
 	const user = await prisma.UserLocal.findFirst({
 		where: { email: email },
 	});
-	// console.log(
-	// 	'[loginUserModel] user after prisma.UserLocal.findFirst: ',
-	// 	user
-	// );
+	console.log(
+		'[loginUserModel] user after prisma.UserLocal.findFirst: ',
+		user
+	);
 	if (!user) {
 		return null;
 	}
 
 	const isPasswordValid = bcrypt.compareSync(password, user.password);
-	// console.log(
-	// 	'[loginUserModel] isPasswordValid after bcrypt.compareSync: ',
-	// 	isPasswordValid
-	// );
+	console.log(
+		'[loginUserModel] isPasswordValid after bcrypt.compareSync: ',
+		isPasswordValid
+	);
 	if (!isPasswordValid) {
 		return null;
 	}
